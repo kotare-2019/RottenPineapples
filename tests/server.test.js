1aaec22a4e3ba6d6edebaf1ebb7afebbb18460ea
danbez13 <daniel.bez13@gmail.com>
2019-06-14 11:30:51 +1200

    update seed data

--- tests/server.test.js
+++ tests/server.test.js
@@ -8,6 +8,11 @@ jest.mock('../db', () => ({
   getUsers: () => Promise.resolve([
     {id: 2, name: 'test user 2', email: 'test2@user.nz'},
     {id: 4, name: 'test user 4', email: 'test4@user.nz'}
+  ]),
+  getListMovies: () => Promise.resolve([
+    {id: 1, title: 'A.I.', year: '2001', director: 'Steven Spielberg'}, 
+    {id: 2, title: 'After Earth', year: '2013', director: 'M. Night Shyamalan'}, 
+    {id: 3, title: 'Alien', year: '1979', director: 'Ridley Scott'}
   ])
 }))
 
