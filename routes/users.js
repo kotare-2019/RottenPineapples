3a6d4d9e69647e42def6f71a83806b03471e0bcb
dan <mikewipa@gmail.com>
2019-06-14 10:42:52 +1200

    Basic CSS

--- routes/users.js
+++ routes/users.js
@@ -14,4 +14,33 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/movies', (req, res) =>{
+  db.getUsers()
+  .then(movies =>{
+    res.render('movies', movies)
+  })
+  .catch(err => {
+    res.status(500).send('DATABASE ERROR: ' + err.message)
+  })
+})
+
+router.get('/ratings', (req, res) =>{
+  db.getUsers()
+  .then(ratings =>{
+    res.render('ratings', ratings)
+  })
+  .catch(err => {
+    res.status(500).send('DATABASE ERROR: ' + err.message)
+  })
+})
+
+router.get('/recomendations', (req, res) =>{
+  db.getUsers()
+  .then(recomendations =>{
+    res.render('recomendations', recomendations)
+  })
+  .catch(err => {
+    res.status(500).send('DATABASE ERROR: ' + err.message)
+  })
+})
 module.exports = router
